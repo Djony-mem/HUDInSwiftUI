3593b77759b1c68bcf54108d0033be08d444d225
brubru <Buyko@MacBook-Pro.local>
2021-03-13 04:05:42 +0300

    first commit

---
+++ HUDInSwiftUI/HUDView.swift
@@ -0,0 +1,20 @@
+//
+//  HUDView.swift
+//  HUDInSwiftUI
+//
+//  Created by brubru on 11.03.2021.
+//
+
+import SwiftUI
+
+struct HUDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HUDView_Previews: PreviewProvider {
+    static var previews: some View {
+        HUDView()
+    }
+}
